fb6d19611ecee662b0fae19ee56809fa51968e17
amityadav0612 <amity7205@gmail.com>
2025-07-27 23:18:31 +0530

    added course endpoint

--- backend/routes/admin/index.js
+++ backend/routes/admin/index.js
@@ -4,7 +4,7 @@ const jwt=require("jsonwebtoken");
 const  { JWT_ADMIN_PASSWORD } = require("../config");
 
 const bcrypt=require('bcrypt');
-const { adminModel, userModel } = require('../../db');
+const { adminModel, userModel, courseModel } = require('../../db');
 const { adminMiddleware } = require('../../middlewares/admin');
 
 adminRouter.post("/signup",async (req,res)=>{
@@ -61,6 +61,20 @@ const adminId=req.body.userId;
 
 const{ title,descrption,imageurl,price}=req.body;
 
+try {
+    const course=await courseModel.create({
+        title,descrption,imageurl,price,createrId:adminId
+    })
+res.status(200).json({
+    message:"Course is created ..",
+    courseId:course._id
+})
+
+} catch (error) {
+    res.status(500).json({
+        message:"Error while creating course.."
+    })
+}
 
 
 
