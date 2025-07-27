55389aaa6601ba262f30af12c77f48ce07f705aa
amityadav0612 <amity7205@gmail.com>
2025-07-27 23:44:24 +0530

    added user middleware

---
+++ backend/middlewares/user.js
@@ -0,0 +1,25 @@
+const Jwt=require("jsonwebtoken");
+
+const JWT_USER_PASSWORD=require("../config");
+
+
+
+function userMiddleware(req,res,next){
+const token=req.headers.token;
+
+const decoded=jwt.verify(token,JWT_USER_PASSWORD);
+
+if(decoded){
+    req.userId=decoded.userId;
+    next();
+}
+else{
+    res.status(403).json({
+        message:"You are not signin.."
+    })
+}
+}
+
+module.exports={
+    userMiddleware
+}
